110876c999304f5cd7811d45a9328953c0a1f8b6
Pedro Pombeiro <pedropombeiro@gmail.com>
2013-05-03 18:37:55 +0200

    Add XML doc support.

--- Class1.cs
+++ Class1.cs
@@ -7,6 +7,35 @@ namespace T4Factories
 {
     public class Class1 : IClass1
     {
+        private readonly int _param1;
+        private readonly DateTime _now;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Class1"/> class.
+        /// </summary>
+        /// <param name="param1">
+        /// The param 1.
+        /// </param>
+        public Class1(int param1)
+        {
+            _param1 = param1;
+            _now = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Class1"/> class.
+        /// </summary>
+        /// <param name="param1">
+        /// The param 1.
+        /// </param>
+        /// <param name="now">
+        /// The now.
+        /// </param>
+        public Class1(int param1, DateTime now)
+        {
+            _param1 = param1;
+            _now = now;
+        }
     }
 
     public interface IClass1
