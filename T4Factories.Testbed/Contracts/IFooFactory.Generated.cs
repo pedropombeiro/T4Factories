708cc09890950bd51bb06a6ff98867c0070f3f92
Pedro Pombeiro <pedropombeiro@gmail.com>
2013-05-09 00:57:16 +0200

    Add support for generic type factories (without constraints).

--- T4Factories.Testbed/Contracts/IFooFactory.Generated.cs
+++ T4Factories.Testbed/Contracts/IFooFactory.Generated.cs
@@ -1,17 +1,17 @@
-﻿namespace T4Factories.Contracts
+﻿namespace T4Factories.Testbed.Contracts
 {
     using System;
     using T4Factories.Models;
 
     [global::System.CodeDom.Compiler.GeneratedCode("T4Factories", "0.1")]
-    public interface IFooFactory
+    public interface IFooFactory<T>
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="Foo"/> class. The object creation timestamp defaults to <see cref="DateTime.Now"/>.
+        /// Initializes a new instance of the <see cref="Foo{T}"/> class. The object creation timestamp defaults to <see cref="DateTime.Now"/>.
         /// </summary>
         /// <param name="bar">
         /// The service responsible for ...
         /// </param>
-        T4Factories.Contracts.IFoo Create(T4Factories.Contracts.IBar bar);
+        T4Factories.Testbed.Contracts.IFoo<T> Create(T bar);
     }
 }
